391f24fb5bb53b88d01252b5eba6f3728f457a54
Vaibhav10sept <vaibhavtiwari10sept@gmail.com>
2021-04-19 13:00:58 +0530

    asdfas

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ function Home() {
     return (
         <div>
             <h1>quiz app</h1>
-            <Link to="/QuizCategories" >quiz app</Link>
+            <Link to="/QuizCategories" >quiz app</Link><br/>
             <Link to="/level1">vocabulary game</Link>
 
         </div>
