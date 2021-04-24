5d0bfabd81793af992289eb8ba07dc03ee881c04
SansakrSachan <Sanskar841signh@gmail.com>
2021-04-24 12:00:43 +0530

    new

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ import '../components/home.css'
 function Home() {
     return (
         <div>
-            <button class="colorful-button">
+            {/* <button class="colorful-button">
     <div class="wrapper">
         <span>QUIZ APP</span>
         <div class="circle circle-12"></div>
@@ -61,10 +61,50 @@ function Home() {
         <div class="circle circle-1"></div>
     </div>
     
-</button></Link>
+</button></Link> */}
 
 
+<div class="flex-container">
+  <div class="flex-item-left"> <Link to="/QuizCategories" ><button class="colorful-button">
+    <div class="wrapper">
+        <span>Take a quiz</span>
+        <div class="circle circle-12"></div>
+        <div class="circle circle-11"></div>
+        <div class="circle circle-10"></div>
+        <div class="circle circle-9"></div>
+        <div class="circle circle-8"></div>
+        <div class="circle circle-7"></div>
+        <div class="circle circle-6"></div>
+        <div class="circle circle-5"></div>
+        <div class="circle circle-4"></div>
+        <div class="circle circle-3"></div>
+        <div class="circle circle-2"></div>
+        <div class="circle circle-1"></div>
+    </div>
+    
+</button>
 
+</Link>
+</div>
+  <div class="flex-item-right">            <Link to="/level1"><button class="colorful-button">
+    <div class="wrapper">
+        <span>Vocablary Game</span>
+        <div class="circle circle-12"></div>
+        <div class="circle circle-11"></div>
+        <div class="circle circle-10"></div>
+        <div class="circle circle-9"></div>
+        <div class="circle circle-8"></div>
+        <div class="circle circle-7"></div>
+        <div class="circle circle-6"></div>
+        <div class="circle circle-5"></div>
+        <div class="circle circle-4"></div>
+        <div class="circle circle-3"></div>
+        <div class="circle circle-2"></div>
+        <div class="circle circle-1"></div>
+    </div>
+    
+</button></Link></div>
+</div>
 
 
 
